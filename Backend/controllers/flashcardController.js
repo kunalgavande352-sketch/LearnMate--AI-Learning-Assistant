06edd4baeb5fc15e09fe35534842766f2f16b09f
kunalgavande352-sketch <kunalgavande352@gmail.com>
2026-05-16 01:18:59 +0530

    flashcard controller & routes

---
+++ Backend/controllers/flashcardController.js
@@ -0,0 +1,163 @@
+import Flashcard from "../models/Flashcard.js";
+
+// @desc Get all flashcard for a document
+// @route GET /api/flashcard/:documents
+// @access Private
+
+export const getFlashcards = async (req,res,next) => {
+    try {
+        const flashcards = await Flashcard.find({
+            userId: req.user._id,
+           documentId: req.params.documentId
+        })
+
+        .populate("documentId","title fileName")
+        .sort({createdAt: -1});
+
+        res.status(200).json({
+            success:true,
+            count:flashcards.length,
+            data:flashcards
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+// @desc Get all flashcard sets for user
+// @route GET /api/flashcard
+// @access Private
+
+export const getAllFlashcardSets = async (req,res,next) => {
+    try {
+        const flashcardSets = await Flashcard.find({userId: req.user._id})
+        .populate('documentId','title')
+        .sort({createdAt: -1})
+
+        res.status(200).json({
+            success:true,
+            count:flashcardSets.length,
+            data:flashcardSets,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+// @desc Mark flashcard as reviewed
+// @route GET /api/flashcard/:carsId/review
+// @access Private
+export const reviewFlashcard = async (req,res,next) => {
+    try {
+        const flashcardSet = await Flashcard.findOne({
+            'cards._id':req.params.cardId,
+            userId:req.user._id
+        });
+
+        if(!flashcardSet){
+            return res.status(404).json({
+                success:false,
+                error:'flashcard set or card not found',
+                statusCode:404
+            })
+        }
+
+        const cardIndex = flashcardSet.cards.findIndex(card => card._id.toString() === req.params.cardId);
+
+        if(cardIndex === -1){
+            return res.status(404).json({
+                success:false,
+                error:'card not found in set',
+                statusCode:404
+            })
+        }
+
+        // Update review info
+        flashcardSet.cards[cardIndex].lastReviewed = new Date();
+        flashcardSet.cards[cardIndex].reviewCount += 1;
+
+        await flashcardSet.save();
+
+        res.status(200).json({
+            success:true,
+            data:flashcardSet,
+            message:"flashcard reviewed successfully"
+        })
+    } catch (error) {
+         next(error)
+    }
+}
+
+// @desc Toggle star/favorite on flashcard
+// @route GET /api/flashcard/:cardId/star
+// @access Private
+
+export const toggleStarFlashcard = async (req,res,next) => {
+    try {
+        const flashcardSet = await Flashcard.findOne({
+            'cards._id':req.params.cardId,
+            userId:req.user._id
+        });
+
+        if(!flashcardSet){
+            return res.status(404).json({
+                success:false,
+                error:'flashcard set or card not found',
+                statusCode:404
+            })
+        }
+
+        const cardIndex = flashcardSet.findIndex(card => card._id.toString() === req.params.cardId);
+
+        if(cardIndex === -1){
+            return res.status(404).json({
+                success:false,
+                error:'card not found in set',
+                statusCode:404
+            })
+        }
+
+        // Toggle star
+
+        flashcardSet.cards[cardIndex].isStarred = !flashcardSet.cards[cardIndex].isStarred;
+
+        await flashcardSet.save();
+
+        res.status(200).json({
+            success:true,
+            data:flashcardSet,
+            message:`Flashcard ${flashcardSet.cards[cardIndex].isStarred ? 'starred' : 'unstarred'}`
+        })
+    } catch (error) {
+         next(error)
+    }
+}
+
+// @desc Delete flashcard set
+// @route GET /api/flashcard/:id
+// @access Private
+
+export const deleteFlashcardSet = async (req,res,next) => {
+    try {
+        const flashcardSet = await Flashcard.findOne({
+            _id: req.params.id,
+            userId: req.user._id
+        });
+
+        if(!flashcardSet){
+            return res.status(404).json({
+                success:false,
+                error:"flashcard set not found",
+                statusCode:404
+            })
+        }
+        await flashcardSet.deleteOne();
+
+        res.status(200).json({
+            success:true,
+            message:"Flashcard set deleted successfully"
+        })
+    } catch (error) {
+         next(error)
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,8 @@ import errorHandler from "./middleware/errorHandler.js"
 
 import authRoutes from "./routes/authRoutes.js"
 import documentRoutes from "./routes/documentRoutes.js"
+import flashcardRoutes from "./routes/flashcardRoutes.js"
+
 
 //ES6 module __dirname alternative
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +42,7 @@ app.use(express.urlencoded({extended:true}));
 //Routes
 app.use("/api/auth",authRoutes)
 app.use('/api/documents',documentRoutes)
+app.use("/api/flashcards",flashcardRoutes)
   
 app.use(errorHandler);
 
